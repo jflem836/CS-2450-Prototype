ade17114fdbd20e99c46edb4f4884c9e1018fd96
JFlem <jordane.fleming.7@gmail.com>
2026-04-28 13:33:23 -0700

    added course database page placeholders and working dropdown

--- functions.js
+++ functions.js
@@ -3,8 +3,13 @@
 // place in button onclick="showScreen('bleh')"
 function showScreen(screenId) {
   document.querySelectorAll(".screen").forEach(screen => {
-    screen.style.display = "none";
+    screen.classList.remove("active");
   });
 
-  document.getElementById(screenId).style.display = "block";
+  document.getElementById(screenId).classList.add("active");
+}
+
+// Brings up the different course cards below major and ge courses on course database screen
+function toggleDropdown(dropdownId) {
+    document.getElementById(dropdownId).classList.toggle("open");
 }
