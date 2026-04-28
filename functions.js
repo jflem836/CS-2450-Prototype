6f1b4859e6bcc0a1cf2340246ebe154550d948cd
JFlem <jordane.fleming.7@gmail.com>
2026-04-28 16:47:16 -0700

    changed format to multiple html files

--- functions.js
+++ functions.js
@@ -1,22 +1,7 @@
-
-// Allows screen to be shown and all others to be hidden
-// place in button onclick="showScreen('bleh')"
-function showScreen(screenId) {
-  console.log("clicked:", screenId);
-
-  document.querySelectorAll(".screen").forEach(screen => {
-    screen.classList.remove("active");
-  });
-
-  const target = document.getElementById(screenId);
-  console.log(target);
-
-  if (target) {
-    target.classList.add("active");
-  }
-}
-
 // Brings up the different course cards below major and ge courses on course database screen
 function toggleDropdown(dropdownId) {
-    document.getElementById(dropdownId).classList.toggle("open");
+  const dropdown = document.getElementById(dropdownId);
+  if (dropdown) {
+    dropdown.classList.toggle("open");
+  }
 }
